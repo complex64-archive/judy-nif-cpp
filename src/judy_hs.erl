1976e883ec7ae73077db07733275f94a26f4b16f
Johannes Huning <mail@johanneshuning.com>
2012-04-26 00:46:12 +0200

    Implement remaining nifs Note that the perfomance hit of term_to_binary has still to be examined

--- src/judy_hs.erl
+++ src/judy_hs.erl
@@ -1,5 +1,5 @@
 -module (judy_hs).
--export ([init/0, new/0, insert/3, delete/2, get/2]).
+-export ([init/0, new/0, insert/3, remove/2, get/2]).
 
 -on_load (init/0).
 
@@ -18,14 +18,10 @@ new() ->
     erlang:nif_error(nif_not_loaded).
 
 
-% TODO - Document!
-insert(JudyArr, Key, Val) when is_binary(Key) andalso is_binary(Val) ->
-    nif_insert(JudyArr, Key, Val);
-
 
 % TODO - Document!
 insert(JudyArr, Key, Val) ->
-    insert(JudyArr, term_to_binary(Key), term_to_binary(Val)).
+    nif_insert(JudyArr, term_to_binary(Key), term_to_binary(Val)).
 
 
 % TODO - Document!
@@ -35,31 +31,28 @@ nif_insert(_JudyArr, _Key, _Value) ->
 
 
 % TODO - Document!
-delete(JudyArr, Key) when is_binary(Key) ->
-    nif_delete(JudyArr, Key);
+remove(JudyArr, Key) ->
+    nif_remove(JudyArr, term_to_binary(Key)).
 
 
 % TODO - Document!
-delete(JudyArr, Key) ->
-    nif_delete(JudyArr, term_to_binary(Key)).
-
-
-% TODO - Document!
-nif_delete(_JudyArr, _Key) ->
+nif_remove(_JudyArr, _Key) ->
     erlang:nif_error(nif_not_loaded).
 
 
 
 % TODO - Document!
-get(JudyArr, Key) when is_binary(Key) ->
-    nif_get(JudyArr, Key);
-
-
-% TODO - Document!
 get(JudyArr, Key) ->
-    get(JudyArr, term_to_binary(Key)).
+    make_value(nif_get(JudyArr, term_to_binary(Key))).
 
 
 % TODO - Document!
 nif_get(_JudyArr, _Key) ->
     erlang:nif_error(nif_not_loaded).
+
+
+
+% TODO - Document!
+make_value({error, Reason}) -> {error, make_value(Reason)};
+
+make_value(Value) -> binary_to_term(Value).
